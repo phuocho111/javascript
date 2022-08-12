ace968646fb26249d5eed80b6ebff166adb98f57
phuocho111 <thanhphuocho11@gmail.com>
2022-08-12 08:43:26 +0700

    update javascript

--- arrow-function.js
+++ arrow-function.js
@@ -1,29 +1,78 @@
-// function
-'use strict'
-let temperature = 30
-let feel;
-if (temperature < 24){
-    feel = function(){
-        console.log("so cool")
-    }
-}else{
-    feel = function(){
-        console.log("so hot")
-    }
-}
-feel()
+// // function
+// 'use strict'
+// let temperature = 30
+// let feel;
+// if (temperature < 24){
+//     feel = function(){
+//         console.log("so cool")
+//     }
+// }else{
+//     feel = function(){
+//         console.log("so hot")
+//     }
+// }
+// feel()
 
-// Arrow function
-'use strict'
-let temperatures = 21
-let feels;
-if(temperatures < 24){
-    feels = () =>{
-        console.log('so cool')
-    }
-}else{
-    feels = () =>{
-        console.log('so hot')
-    }
+// // Arrow function
+// 'use strict'
+// let temperatures = 21
+// let feels;
+// if(temperatures < 24){
+//     feels = () =>{
+//         console.log('so cool')
+//     }
+// }else{
+//     feels = () =>{
+//         console.log('so hot')
+//     }
+// }
+// feels()
+
+
+
+
+// this
+/// function
+
+
+function Courses(name,price){
+    this.name = name;
+    this.price = price
+    console.log(`${this.name},price is ${this.price} $`)
 }
-feels()
+
+
+
+Courses("html",100)
+
+
+///Arrow function
+
+let Course = (name,price)=>{
+    this.name = name;
+    this.price = price
+    console.log(`${this.name},price is ${this.price} $`)
+}
+
+Course("php",200)
+
+
+
+//bind arguments
+
+///function
+let number = function(){
+    console.log(arg)
+}
+number(1,2,3)
+//arg(3)[1,2,3]
+
+///arrow function
+let x = () => {
+    console.log(args);
+}
+
+x(4,6,7); 
+//args is not defined
+
+
